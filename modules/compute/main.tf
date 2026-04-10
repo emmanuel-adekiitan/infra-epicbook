a12c9289af9579812c9e4f7546965a63cac318f0
Emmanuel Adekiitan <aopemma@gmail.com>
2026-04-10 12:16:19 +0100

    Add backend VM resource to compute module

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -51,3 +51,43 @@ resource "azurerm_linux_virtual_machine" "frontend" {
     version   = "latest"
   }
 }
+
+# 1. Backend Network Interface
+resource "azurerm_network_interface" "backend" {
+  name                = "backend-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.backend_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# 2. Backend VM Resource
+resource "azurerm_linux_virtual_machine" "backend" {
+  name                = "vm-backend"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.backend.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
