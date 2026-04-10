9b47d8217b3489c452e4f5c88916b40290c232c0
Emmanuel Adekiitan <aopemma@gmail.com>
2026-04-10 15:52:21 +0100

    modified IP allocation to Static for Standard

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "frontend-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Network Interfaces
